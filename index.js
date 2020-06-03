7bf9461483568685f4c65db1d740a0b590c73b7a
silvanus <selvasilvanus@gmail.com>
2020-06-03 16:41:40 +0530

    fourth commit

--- index.js
+++ index.js
@@ -5,39 +5,39 @@ const path = require('path')
 const hostname = "localhost"
 const port = 3000
 
-const server = http.createServer((req,res) => {
+const server = http.createServer((req, res) => {
     console.log(`request for ${req.url} by method ${req.method}`);
-    if(req.method == "GET"){
-        var fileUrl;
-        if(req.url == "/") fileUrl = "/index.html"
+    if (req.method == "GET") {
+        var fileUrl, filepath, fileExt;
+
+        if (req.url == "/") fileUrl = "/index.html"   // or just "index.html" 
         else fileUrl = req.url;
-        
-        var filepath = path.resolve("./" + fileUrl);
-        const fileExt = path.extname(filepath);
-        
-        if(fileExt == ".html"){
+        filepath = path.resolve("./" + fileUrl);
+        fileExt = path.extname(filepath);
+
+        if (fileExt == ".html") {
             fs.exists(filepath, (exists) => {
-                if(!exists){
+                if (!exists) {
                     res.statusCode = 404;
-                    res.setHeader("content-type" ,"text/html");
+                    res.setHeader("content-type", "text/html");
                     res.end(`<html><body> <h1>Error : 404 ${fileUrl} not found.</h1> </body></html>`);
                 }
-                else{
+                else {
                     res.statusCode = 200;
-                    res.setHeader("content-type" ,"text/html");
+                    res.setHeader("content-type", "text/html");
                     fs.createReadStream(filepath).pipe(res);
                 }
             });
         }
-        else{
+        else {
             res.statusCode = 404;
-             res.setHeader("content-type" ,"text/html");
+            res.setHeader("content-type", "text/html");
             res.end(`<html><body> <h1>Error : 404 ${fileUrl} not a html file.</h1> </body></html>`);
         }
     }
-    else{
+    else {
         res.statusCode = 404;
-         res.setHeader("content-type" ,"text/html");
+        res.setHeader("content-type", "text/html");
         res.end(`<html><body> <h1>Error : 404 ${req.method} not supported.</h1> </body></html>`);
     }
 })
